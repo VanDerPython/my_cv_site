5686e0877a52c77cb5478b87b1d9399f746ef33b
VanDerPython <vanderpythondevelopment@gmail.com>
2022-05-14 17:12:55 +0300

    little py refactor

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template, redirect, request, url_for,abort
+from flask import Flask, render_template, redirect, request, url_for, abort
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
@@ -6,27 +6,32 @@ from wtforms import StringField, SubmitField, FileField, PasswordField
 from flask_ckeditor import CKEditor, CKEditorField
 from wtforms.validators import DataRequired
 import sqlite3
-from flask_login import UserMixin,login_user,LoginManager,login_required,current_user,logout_user
+from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
 from functools import wraps
-from werkzeug.security import  generate_password_hash, check_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 import datetime as dt
 import os
 import re
 SECRET_KEY = os.urandom(32)
+
+
 def admin_only(f):
     @wraps(f)
-    def decorated_function(*args,**kwargs):
-        if current_user.id !=1:
+    def decorated_function(*args, **kwargs):
+        if current_user.id != 1:
             return abort(403)
-        return f(*args,**kwargs)
+        return f(*args, **kwargs)
     return decorated_function
 # Forms
+
+
 class JobForm(FlaskForm):
     place = StringField("Место работы", validators=[DataRequired()])
     date = StringField("Срок работы", validators=[DataRequired()])
-    position = StringField("Должность",validators=[DataRequired()], )
+    position = StringField("Должность", validators=[DataRequired()], )
     achievements = CKEditorField("Осуществленные компетенции", validators=[DataRequired()])
     submit = SubmitField("Подтвердить")
+
 
 class BlogForm(FlaskForm):
     post_title = StringField("Заголовок", validators=[DataRequired()])
@@ -36,15 +41,17 @@ class BlogForm(FlaskForm):
     body = CKEditorField("Пост", validators=[DataRequired()])
     submit = SubmitField("Подтвердить")
 
+
 class RegisterForm(FlaskForm):
-    name = StringField("Никнейм",validators=[DataRequired()])
+    name = StringField("Никнейм", validators=[DataRequired()])
     email = StringField("Адрес эл.почты", validators=[DataRequired()])
-    password = StringField("Пароль",validators=[DataRequired()])
+    password = StringField("Пароль", validators=[DataRequired()])
     submit = SubmitField("Подтвердить")
+
 
 class LoginForm(FlaskForm):
     email = StringField("Адрес эл.почты", validators=[DataRequired()])
-    password = PasswordField("Пароль",validators=[DataRequired()])
+    password = PasswordField("Пароль", validators=[DataRequired()])
     submit = SubmitField("Подтвердить")
 
 
@@ -57,41 +64,46 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
+
 #Коррекция базы данных (УДАЛИТЬ)
 # db_correction = sqlite3.connect("jobs.db")
 # cursor = db_correction.cursor()
 # cursor.execute(f"ALTER TABLE job_post ADD COLUMN font_awesome 'string'")
-ckeditor =CKEditor(app)
+ckeditor = CKEditor(app)
 app.config["SECRET_KEY"] = SECRET_KEY
+
 
 @login_manager.user_loader
 def load_user(user_id):
     return Users.query.get(int(user_id))
 #DB TABLE
+
+
 class JobPost(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    job_title = db.Column(db.String(250),unique=False,nullable=False)
-    working_time = db.Column(db.String(250), unique = False, nullable = False)
-    achievements = db.Column(db.String(250),unique = False,nullable = False)
-    position = db.Column(db.String(6000), unique = False, nullable = False)
+    id = db.Column(db.Integer, primary_key=True)
+    job_title = db.Column(db.String(250), unique=False, nullable=False)
+    working_time = db.Column(db.String(250), unique=False, nullable=False)
+    achievements = db.Column(db.String(250), unique=False, nullable=False)
+    position = db.Column(db.String(6000), unique=False, nullable=False)
+
 
 class BlogPost(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    post_title = db.Column(db.String(250), unique = True, nullable = False)
-    post_subtitle = db.Column(db.String(250), unique = True, nullable = False)
-    author = db.Column(db.String(250), unique = False,nullable = False)
-    date = db.Column(db.String(250), unique = False, nullable = False)
-    topic = db.Column(db.String(250), unique = False, nullable = False)
-    body = db.Column(db.String(250), unique = True, nullable = False)
+    id = db.Column(db.Integer, primary_key=True)
+    post_title = db.Column(db.String(250), unique=True, nullable=False)
+    post_subtitle = db.Column(db.String(250), unique=True, nullable=False)
+    author = db.Column(db.String(250), unique=False, nullable=False)
+    date = db.Column(db.String(250), unique=False, nullable=False)
+    topic = db.Column(db.String(250), unique=False, nullable=False)
+    body = db.Column(db.String(250), unique=True, nullable=False)
+
 
 class Users(UserMixin, db.Model):
-    id = db.Column(db.Integer,primary_key = True)
-    name = db.Column(db.String(250), unique = False, nullable = False)
-    email = db.Column(db.String(250), unique = False, nullable = False)
-    password = db.Column(db.String(250), unique = False, nullable = False)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), unique=False, nullable=False)
+    email = db.Column(db.String(250), unique=False, nullable=False)
+    password = db.Column(db.String(250), unique=False, nullable=False)
 
 # db.create_all()
-
 
 
 @app.route("/")
@@ -101,40 +113,42 @@ def start():
 
 @app.route("/index")
 def index():
-    return render_template("index.html",is_loggedin = current_user.is_authenticated)
+    return render_template("index.html", is_loggedin=current_user.is_authenticated)
 
-@app.route("/register",methods=["GET","POST"])
+
+@app.route("/register", methods=["GET", "POST"])
 def register():
     form = RegisterForm()
     if request.method == "POST":
         hash_and_salted_password = generate_password_hash(
             request.form.get("password"),
-            method = "pbkdf2:sha256",
+            method="pbkdf2:sha256",
             salt_length=8
         )
         new_user = Users(
-            name = form.name.data,
-            email = form.email.data,
-            password = hash_and_salted_password
+            name=form.name.data,
+            email=form.email.data,
+            password=hash_and_salted_password
 
         )
         db.session.add(new_user)
         db.session.commit()
         login_user(new_user)
         return redirect(url_for('blog'))
-    return render_template("register.html", form=form,is_loggedin = current_user.is_authenticated)
+    return render_template("register.html", form=form, is_loggedin=current_user.is_authenticated)
 
-@app.route("/login", methods = ["GET","POST"])
+
+@app.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
     if request.method == "POST":
         email = request.form.get("email")
         password = request.form.get("password")
-        user = Users.query.filter_by(email = email).first()
+        user = Users.query.filter_by(email=email).first()
         if check_password_hash(user.password, password):
             login_user(user)
             return redirect(url_for("index"))
-    return render_template("login.html", form = form,is_loggedin = current_user.is_authenticated)
+    return render_template("login.html", form=form, is_loggedin=current_user.is_authenticated)
 
 
 @app.route('/logout')
@@ -142,26 +156,31 @@ def logout():
     logout_user()
     return redirect(url_for('index'))
 
-@app.route("/add_job", methods = ["GET","POST"])
+
+@app.route("/add_job", methods=["GET", "POST"])
 @admin_only
 def add_job():
     form = JobForm()
     if form.validate_on_submit():
         new_job = JobPost(
-            job_title = form.place.data,
-            working_time = form.date.data,
-            achievements = form.achievements.data,
-            position = form.position.data
+            job_title=form.place.data,
+            working_time=form.date.data,
+            achievements=form.achievements.data,
+            position=form.position.data
         )
         db.session.add(new_job)
         db.session.commit()
         return redirect(url_for("about"))
-    return render_template("add_job.html", form=form, current_user=current_user,is_loggedin = current_user.is_authenticated)
+    return render_template("add_job.html",
+                           form=form, current_user=current_user,
+                           is_loggedin=current_user.is_authenticated)
+
 
 @app.route("/about")
 def about():
     posts = JobPost.query.all()
-    return render_template("about.html", posts = posts,is_loggedin = current_user.is_authenticated)
+    return render_template("about.html", posts=posts, is_loggedin=current_user.is_authenticated)
+
 
 @app.route("/job<int:job_post>")
 def job(job_post):
